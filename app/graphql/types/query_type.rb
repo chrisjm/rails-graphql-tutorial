b109cb05013b57ac88d28e616c53eba7f5b980e7
Chris Mears <chris.mears@gmail.com>
2017-12-06 10:35:32 -0800

    Create Link type and allLinks query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,10 @@
 Types::QueryType = GraphQL::ObjectType.define do
-  name "Query"
+  name 'Query'
   # Add root-level fields here.
   # They will be entry points for queries on your schema.
 
-  # TODO: remove me
-  field :testField, types.String do
-    description "An example field added by the generator"
-    resolve ->(obj, args, ctx) {
-      "Hello World!"
-    }
+  field :allLinks, !types[Types::LinkType] do
+    # resolve would be called in order to fetch data for that field
+    resolve ->(_obj, _args, _ctx) { Link.all }
   end
 end
