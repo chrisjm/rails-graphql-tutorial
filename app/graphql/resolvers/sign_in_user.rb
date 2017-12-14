39a436b56b36b413237263ec4eea47721c7467b2
Chris Mears <chris.mears@gmail.com>
2017-12-14 12:51:07 -0800

    Add SignInUser mutation 🔑

---
+++ app/graphql/resolvers/sign_in_user.rb
@@ -0,0 +1,34 @@
+class Resolvers::SignInUser < GraphQL::Function
+  argument :email, !Types::AuthProviderEmailInput
+
+  # defines the inline type for the mutation
+  type do
+    name 'SignInPayload'
+
+    field :token, types.String
+    field :user, Types::UserType
+  end
+
+  def call(_obj, args,_ctx)
+    input = args[:email]
+
+    # basic validation
+    return unless input
+
+    user = User.find_by email: input[:email]
+
+    # ensure correct user
+    return unless user && user.authenticate(input[:password])
+
+    # build token
+    crypt = ActiveSupport::MessageEncryptor.new(
+      Rails.application.secrets.secret_key_base.byteslice(0..31)
+    )
+    token = crypt.encrypt_and_sign("user-id:#{user.id}")
+
+    OpenStruct.new(
+      user: user,
+      token: token
+    )
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,4 +3,5 @@ Types::MutationType = GraphQL::ObjectType.define do
 
   field :createLink, function: Resolvers::CreateLink.new
   field :createUser, function: Resolvers::CreateUser.new
+  field :signInUser, function: Resolvers::SignInUser.new
 end
